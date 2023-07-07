474b530333ac9819cb4d197dfce09980bcf5903e
Cyx <cyxkris6@gmail.com>
2023-07-07 14:13:17 +0100

    Polishing.

--- src/js/bg-change.js
+++ src/js/bg-change.js
@@ -1,14 +1,19 @@
 
+// Function for when the window is scrolled
 $(window).scroll(function () {
 
+    // Selecting the body
     $('#body').each(function () {
 
+        // Getting the position of the top and bottom of the window
         var topOfWindow = $(window).scrollTop(),
 
             bottomOfWindow = topOfWindow + $(window).height();
 
+        // Getting the position of the footer
         var boxPos = $('#footer').offset().top;
 
+        // Changing the background color of the body based on the position of the footer.
         if (boxPos <= bottomOfWindow - 100 && boxPos >= topOfWindow) {
             $("#body").css("background-color", "#1D2226");
         } else {
@@ -21,8 +26,5 @@ $(window).scroll(function () {
 
 });
 
-$(function () {
-    $('[data-toggle="popover"]').popover()
-})
 
 
